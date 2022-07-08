931c45d186c73311d663587b9c48139ab3ce0656
Victor Manuel Diaz Valdivia <victor.diaz@elecssp.une.cu>
2022-07-08 14:18:41 -0400

    Agregando las motherboard y los micros

--- Controllers/MicroController.cs
+++ Controllers/MicroController.cs
@@ -18,4 +18,160 @@ public class MicroController : Controller
         return View(context.MicroProcesadores);
     }
 
+    public IActionResult TodosMicros()
+    {
+        return View("TodosMicros", context.MicroProcesadores);
+    }
+
+
+     public IActionResult Crear()
+    {
+      return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult Crear(String NumSerieId, string Marca, string Tecnologia, Estado estado)
+    {
+        try
+        {
+
+            List<MicroProcesador> NuevoMicro = new List<MicroProcesador>();
+            NuevoMicro.Add(new MicroProcesador() { NumSerieId = NumSerieId.ToLower(), Marca = Marca.ToUpper(), Tecnologia = Tecnologia.ToUpper(), estado = estado });
+
+            context.MicroProcesadores.AddRange(NuevoMicro);
+            context.SaveChanges();
+
+            return View("Index", NuevaMemoria.FirstOrDefault());
+
+
+        }
+        catch (Exception ex)
+        {
+
+            return View(ex.Message);
+        }
+
+
+    }
+
+    public IActionResult Editar()
+    {
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Editar(String NumSerieId, string Marca, string Tecnologia, Estado estado)
+    {
+        try
+        {
+
+            List<MicroProcesador> NuevoMicro = new List<MicroProcesador>();
+            NuevoMicro.Add(new MicroProcesador() { NumSerieId = NumSerieId.ToLower(), Marca = Marca.ToUpper(), Tecnologia = Tecnologia.ToUpper(), estado = estado });
+
+            context.MicroProcesadores.UpdateRange(NuevoMicro);
+            context.SaveChanges();
+
+            return View("TodosMicros", NuevaMemoria.FirstOrDefault());
+
+
+        }
+        catch (Exception ex)
+        {
+
+            return View(ex.Message);
+        }
+
+
+    }
+
+    public IActionResult Borrar()
+    {
+
+        return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult Borrar(MicroProcesador micro)
+    {
+        try
+        {
+            context.MicroProcesadores.Remove(micro);
+
+            context.SaveChanges();
+
+            return View("TodosMicros", context.MicroProcesadores);
+        }
+        catch (Exception)
+        {
+
+            return View(ex.Message);
+        }
+
+
+    }
+
+
+    public IActionResult BuscarSerie()
+    {
+        return View("BuscarSerie");
+    }
+
+    [HttpPost]
+    public IActionResult BuscarSerie(MicroProcesador micro)
+    {
+
+        try
+        {
+            List<MicroProcesador> buscadorMicro = new List<MicroProcesador>();
+
+            var buscarMicro = from micro in context.MicroProcesadores
+                              where micro == micro
+                              select micro;
+
+            buscadorMicro.AddRange(BuscarMicro);
+
+            return View("Index", buscadorMicro.FirstOrDefault());
+        }
+        catch (Exception ex)
+        {
+
+            return View(ex.Message);
+        }
+
+    }
+
+
+    public IActionResult BuscarMarca()
+    {
+        return View();
+    }
+
+
+
+    [HttpPost]
+    public IActionResult BuscarMarca(string marca)
+    {
+
+        try
+        {
+            IEnumerable<MemoriaRam> buscarMicro = from micro in context.MicroProcesadores
+                                                  where micro.Marca.Substring(0, 3).ToUpper() == marca.Substring(0, 3).ToUpper()
+                                                  select micro;
+
+            return View("TodosMicros", buscarMicro.ToList());
+        }
+        catch (Exception ex)
+        {
+
+            return View(ex.Message);
+        }
+
+    }
+
+
+
+
 }
