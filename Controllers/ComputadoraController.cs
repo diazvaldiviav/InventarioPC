9deb4c6f162a5eb974575b9d9e2aa8edf2252694
Victor Diaz <diazvaldiviav@gmail.com>
2022-07-10 22:01:35 -0400

    cambiando a pruebabd

--- Controllers/ComputadoraController.cs
+++ Controllers/ComputadoraController.cs
@@ -26,6 +26,173 @@ namespace ProyectoInventarioASP.Controllers
             return View(await computadoraContext.ToListAsync());
         }
 
+        public IActionResult Filter()
+        {
+            return View();
+        }
+
+
+       //Interaccion con el formulario de busqueda a travez del metodo post
+    [HttpPost]
+    //pasamos el inv primero como parametro
+    public IActionResult Filter(string NumInvId)
+    {
+        try
+        {
+
+
+            //aqui buscamos el id a traves del contexto
+            var validacion = _context.Computadoras.Find(NumInvId);
+            //valido el dato que me pasaron como parametro es null si no es null pues lo que nos entraron fue el id
+            if (validacion != null)
+            {
+                //si no es null hacemos la consulta a la base de datos buscando el id
+                var buscadorPC = from pc in _context.Computadoras
+                                 where pc.NumInvId == NumInvId
+                                 select pc;
+
+                //aqui devolvemos nuestro resultado a la vista
+                return View("Index",buscadorPC);
+            }
+            else
+            {
+                //validacion del nombre
+                IEnumerable<Computadora> BuscarPc = from pc in _context.Computadoras
+                                                    where pc.Nombre.ToUpper() == NumInvId.ToUpper()
+                                                    select pc;
+
+                var validacionNombre = BuscarPc.ToArray();
+
+                if (validacionNombre.Length != 0)
+                {
+                    return View("Details", BuscarPc);
+                }
+                else
+                {
+                    //validacion del nombre del departamento
+                    IEnumerable<Computadora> BuscarNomDep = from pc in _context.Computadoras
+                                                            where pc.NombreDepartamento.ToUpper() == NumInvId.ToUpper()
+                                                            select pc;
+
+                    var validacionNomDep = BuscarNomDep.ToArray();
+
+                    if (validacionNomDep.Length != 0)
+                    {
+                        return View("Index", BuscarNomDep.ToList());
+                    }
+                    else
+                    {
+                        //validacion del nombre area
+                        IEnumerable<Computadora> BuscarNomArea = from pc in _context.Computadoras
+                                                                 where pc.NombreArea.ToUpper() == NumInvId.ToUpper()
+                                                                 select pc;
+
+                        var validacionNomArea = BuscarNomArea.ToArray();
+
+                        if (validacionNomArea.Length != 0)
+                        {
+                            return View("Index", BuscarNomArea.ToList());
+                        }
+
+                        else
+                        {
+                            //validacion del nombre del usuario
+                            IEnumerable<Computadora> BuscarNomUsuario = from pc in _context.Computadoras
+                                                                        where pc.NombreUsuarioId.ToLower() == NumInvId.ToLower()
+                                                                        select pc;
+
+                            var validacionNomUsua = BuscarNomUsuario.ToArray();
+
+                            if (validacionNomUsua.Length != 0)
+                            {
+                                return View("Details", BuscarNomUsuario.FirstOrDefault());
+                            }
+                            //Aqui comienzo a filtrar a travez de las relaciones de otras tablas
+                            //Comienzo validando la memoria Ram a travez del id y la tecnologia
+                            else
+                            {
+                                //Comienzo por la capacidad
+                                //Selecciono el id a travez de la Capacidad
+                                var BuscarMem = from mem in _context.MemoriasRam
+                                                where mem.Capacidad == NumInvId
+                                                select mem.NumSerieId;
+
+                                //Paso el id o los ids que obtengo a un array
+                                var RamString = BuscarMem.ToArray();
+
+                                //valido que el array tenga algo
+                                if (RamString.Length != 0)
+                                {
+                                    
+                                    List<Computadora> NuevaLista = new List<Computadora>();
+                                     //si el array tiene algun objeto lo recorro para iterar por cada objeto 
+                                    for (int i = 0; i < RamString.Length; i++)
+                                    {
+                                        var JoinPcRam = from pc in _context.Computadoras
+                                                        join ram in _context.MemoriasRam
+                                                        on pc.MemoriaRamId equals ram.NumSerieId
+                                                        where pc.MemoriaRamId == RamString[i]
+                                                        select pc;
+                                    //guardamos los objetos en una lista creada anteriormente
+                                        NuevaLista.AddRange(JoinPcRam);
+                                    }
+                                    //aqui le muestro el resultado de la lista a la vista
+                                    return View("Index", NuevaLista);
+
+
+                                }
+                                else
+                                {
+                                    var BuscarMemTec = from mem in _context.MemoriasRam
+                                                       where mem.Tecnologia.ToUpper() == NumInvId.ToUpper()
+                                                       select mem.NumSerieId;
+
+                                    var RamStringTec = BuscarMemTec.ToArray();
+
+                                    if (RamStringTec.Length != 0)
+                                    {
+                                        List<Computadora> NuevaListaTec = new List<Computadora>();
+
+                                        for (int i = 0; i < RamStringTec.Length; i++)
+                                        {
+                                            var JoinPcRamTec = from pc in _context.Computadoras
+                                                               join ram in _context.MemoriasRam
+                                                               on pc.MemoriaRamId equals ram.NumSerieId
+                                                               where pc.MemoriaRamId == RamStringTec[i]
+                                                               select pc;
+
+                                            NuevaListaTec.AddRange(JoinPcRamTec);
+                                        }
+
+                                        return View("Index", NuevaListaTec);
+
+                                    }
+                                    //A partir de aqui comenzare a validar por disco duro
+                                    //por capacidad y tecnologia al igual que la Ram 
+                                    else
+                                    {
+                                        
+                                        
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            return View();
+        }
+
+        return View();
+    }
+
+
         // GET: Computadora/Details/5
         public async Task<IActionResult> Details(string id)
         {
